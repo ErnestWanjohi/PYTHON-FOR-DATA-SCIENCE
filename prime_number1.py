ac745e755da010c5093ba9f78347fd46db622251
ErnestWanjohi <126015109+ErnestWanjohi@users.noreply.github.com>
2024-02-01 18:20:56 +0300

    Create prime_number1.py
    outputs prime numbers in a range of values

---
+++ prime_number1.py
@@ -0,0 +1,25 @@
+def isPrime(x):
+    if x < 2:
+        return False
+    elif x == 2:
+        return True
+    for n in range(2, x):
+        if x % n ==0:
+            return False
+    return True
+#test data
+#print (isPrime(3))
+
+#inn a range of values
+def primeGenerator(a, b):
+    for z in range(a, b):
+
+        if isPrime(z):
+            yield z
+    # your code goes here
+
+
+f = int(input())
+t = int(input())
+
+print(list(primeGenerator(f, t)))
